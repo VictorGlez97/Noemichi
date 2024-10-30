e8b812827ba80d229f061ab3f70b21982d85d80f
vhgonzalez <vh.gonzalez@mavi.mx>
2024-10-30 13:53:53 -0600

    Agregando componentes al menu

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+import NoemichiService from '../services/noemichi';
+import NoemichiBakery from '../assets/img/noemichis.png';
 
 export const Menu = () => {
   
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        NoemichiService.get('product')
+        .then(res => {
+            setProducts(res.data)
+        })
+        .catch(error => {
+            console.log( error );
+        })
+    }, []);
+
     return (
         <div>
-            
+            <div className='col-12 flex'>
+                <div className='justify-content-center'>
+                    <img src={NoemichiBakery} alt='Noemichis bakery' style={{ width: '5rem', height: '5rem' }} />
+                </div>
+
+                <div>
+                    <pre>
+                        {{ products }}
+                    </pre>
+                </div>
+
+            </div>
         </div>
     )
 }
--- src/services/noemichi.js
+++ src/services/noemichi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const NoemichiService = axios.create({
-    baseURL: '',
+    baseURL: 'https://pruebanode-victorglez97-victorglez97s-projects.vercel.app/api/v1/',
     timeout: 1000,
     headers: {
         'Content-Type': 'application/json'
