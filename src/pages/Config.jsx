3237985e7922ea4e920d229a9dbc067c7fed8717
vhgonzalez <vh.gonzalez@mavi.mx>
2025-01-29 11:48:49 -0600

    arreglando errores en modulo de configuracion

--- src/pages/Config.jsx
+++ src/pages/Config.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import api from '../services/noemichi';
 
 import { InputText } from 'primereact/inputtext'
@@ -24,7 +24,7 @@ const Config = () => {
 
         console.log( response );
 
-        if ( response.data.data && !response.data.error ) {
+        if ( response.data !== undefined && response.data.data !== undefined && !response.data.data.error ) {
             setConfigurations(response.data.data);
         }
 
@@ -43,16 +43,16 @@ const Config = () => {
             const response = await api.put(`config/${item.idconfig}`, item);
             console.log( response );
 
-            if ( response.data.data ) {
+            // if ( response.data.data ) {
                 
-                if ( response.data.status === 200 && !response.data.data.error ) {
+                // if ( response.data.data !== undefined && response.data.msg === 'Ok' ) {
                     getConfigs();
-                    toast.current.show({ severity: 'success', summary: 'Éxito', detail: 'Configuración actualizada!', life: 3000 });
-                    return;   
-                }
-            }
+                //     toast.current.show({ severity: 'success', summary: 'Éxito', detail: 'Configuración actualizada!', life: 3000 });
+                //     return;   
+                // }
+            // }
 
-            toast.current.show({ severity: 'error', summary: 'Error', detail: res.data.data.mensaje, life: 3000 });
+            // toast.current.show({ severity: 'error', summary: 'Error', detail: res.data.data.mensaje, life: 3000 });
 
         } catch (error) {
             toast.current.show({ severity: 'error', summary: 'Error', detail: error, life: 3000 });
